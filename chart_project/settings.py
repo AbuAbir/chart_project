5b8a0c08d1edf794728a6684b0f8677177a1f1cc
abirhasan100 <a.abir@wsu.edu>
2020-09-17 22:48:24 -0700

    updated settings

--- chart_project/settings.py
+++ chart_project/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%866pnt8mllfg8#4r*n@y7d7o==n7+26l7esx*w-=87#-mqbp)'
+#SECRET_KEY = '%866pnt8mllfg8#4r*n@y7d7o==n7+26l7esx*w-=87#-mqbp)'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mychartapp.herokuapp.com']
 
 
 # Application definition
